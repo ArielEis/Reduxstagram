024190322f523189f10061461e0d23d05ba38874
ArielEis <arieleis1989@gmail.com>
2017-02-22 21:05:37 +0200

    change posts and style

--- src/data/posts.js
+++ src/data/posts.js
@@ -8,13 +8,45 @@ const posts = [
         "e35/12552326_495932673919321_1443393332_n.jpg"
     },
     {
-        "code":"BAcyDyQwcXX2",
+        "code":"BAcyDyQwcXX",
         "caption":"Lunch #hamont",
-        "likes":27,
+        "likes":56,
         "id":"1161022966406956503",
         "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/" +
         "e35/12552326_495932673919321_1443393332_n.jpg"
-    }
+    },
+    {
+        "code":"BAcyDyQwcXX",
+        "caption":"Lunch #hamont",
+        "likes":56,
+        "id":"1161022966406956503",
+        "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/" +
+        "e35/12552326_495932673919321_1443393332_n.jpg"
+    },
+    {
+        "code":"BAcyDyQwcXX",
+        "caption":"Lunch #hamont",
+        "likes":56,
+        "id":"1161022966406956503",
+        "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/" +
+        "e35/12552326_495932673919321_1443393332_n.jpg"
+    },
+    {
+        "code":"BAcyDyQwcXX",
+        "caption":"Lunch #hamont",
+        "likes":56,
+        "id":"1161022966406956503",
+        "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/" +
+        "e35/12552326_495932673919321_1443393332_n.jpg"
+    },
+    {
+        "code":"BAcyDyQwcXX",
+        "caption":"Lunch #hamont",
+        "likes":56,
+        "id":"1161022966406956503",
+        "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/" +
+        "e35/12552326_495932673919321_1443393332_n.jpg"
+    },
 ];
 
 export default posts;
